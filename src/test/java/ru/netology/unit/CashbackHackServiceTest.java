090a04913f457a6c923e677767ee718a117e412c
nvbosova <nv@bosova.pro>
2019-12-04 13:01:04 +0300

    Добавили код сервиса

---
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -0,0 +1,12 @@
+package ru.netology.unit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void () {
+    }
+}
