a7077308146515cc596ee07650c65f47f2b31645
nvbosova <nv@bosova.pro>
2019-12-04 14:05:40 +0300

    Add test ShouldReturn100InAmountIs900

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -7,6 +7,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class CashbackHackServiceTest {
 
     @Test
-    void () {
+    void ShouldReturn100InAmountIs900() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expect = 100;
+
+        assertEquals(expect,actual);
     }
 }
