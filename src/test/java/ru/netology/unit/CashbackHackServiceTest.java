69f509b8043004d8e403d4d379668bada4893953
nvbosova <nv@bosova.pro>
2019-12-04 14:10:00 +0300

    Add test ShouldReturn0InAmountIs1000 (Bug)

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -16,4 +16,15 @@ class CashbackHackServiceTest {
 
         assertEquals(expect,actual);
     }
+
+    @Test
+    void ShouldReturn0InAmountIs1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expect = 0;
+
+        assertEquals(expect,actual);
+    }
 }
